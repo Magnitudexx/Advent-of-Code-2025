1c6b4b3a19d5e6e3c3f5528302adadb4c7658356
Magnitudexx <63145813+Magnitudexx@users.noreply.github.com>
2025-12-01 13:03:41 +0000

    Apply formatting

--- aoc/day01.py
+++ aoc/day01.py
@@ -1,18 +1,22 @@
-
 def parse_input(path: str):
     with open(path) as f:
         return f.read().strip().splitlines()
 
-def specific_parse(data:list[str]):
-    return [(n[0],int(n[1:])) for n in data]
+
+def specific_parse(data: list[str]):
+    return [(n[0], int(n[1:])) for n in data]
+
+
 class Dial:
-    def __init__(self, start:int, maxi:int, data:list[tuple[str,int]]):
+    def __init__(self, start: int, maxi: int, data: list[tuple[str, int]]):
         self._state = start
-        self._maxi = maxi +1
+        self._maxi = maxi + 1
         self._data = data
         self._index = 0
+
     def __iter__(self):
         return self
+
     def __next__(self):
         if self._index < len(self._data):
             rot, val = self._data[self._index]
@@ -20,14 +24,14 @@ class Dial:
             old_state = self._state
             passes = 0
             match rot:
-                case 'L':
+                case "L":
                     self._state = (self._state - val) % self._maxi
                     while val > 0:
                         val -= 1
                         old_state -= 1
                         if old_state % self._maxi == 0:
                             passes += 1
-                case 'R':
+                case "R":
                     self._state = (self._state + val) % self._maxi
                     while val > 0:
                         val -= 1
@@ -38,26 +42,28 @@ class Dial:
         else:
             raise StopIteration
 
+
 def part1(data):
-    p_data:list[tuple[str,int]] = specific_parse(data)
-    n:int = 0
-    
+    p_data: list[tuple[str, int]] = specific_parse(data)
+    n: int = 0
+
     for state, _ in Dial(50, 99, p_data):
         if state == 0:
-            n += 1 
+            n += 1
     return n
 
+
 def part2(data):
-    p_data:list[tuple[str,int]] = specific_parse(data)
-    n:int = 0
-    
+    p_data: list[tuple[str, int]] = specific_parse(data)
+    n: int = 0
+
     for state, rots in Dial(50, 99, p_data):
-            n += rots
+        n += rots
     print(n)
     return n
+
 
 if __name__ == "__main__":
     data = parse_input("input.txt")
     print("Part 1:", part1(data))
     print("Part 2:", part2(data))
-
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-
 # ▶️ main.py (Runner CLI)
 import importlib
 from rich import print
+
 
 def run_day(day: int):
     mod = importlib.import_module(f"aoc.day{day:02}")
@@ -10,13 +10,15 @@ def run_day(day: int):
     print("[cyan]Part 1:[/cyan]", mod.part1(data))
     print("[cyan]Part 2:[/cyan]", mod.part2(data))
 
+
 def cli():
     import sys
+
     if len(sys.argv) < 2:
         print("[red]Usage:[/red] aoc <day>")
         return
     run_day(int(sys.argv[1]))
 
+
 if __name__ == "__main__":
     cli()
-
