27cd8fbe3dd9709a45d9dc0fe6501a735bf8f81d
Philip Lundström <philip.lundstrom@live.se>
2025-12-03 09:18:06 +0100

    done with day 3 but ugly solution

--- aoc/day03.py
+++ aoc/day03.py
@@ -28,17 +28,20 @@ def part1(data):
 def part2(data):
     b = []
     for bank in data:
-        last = len(bank) - 1
-        for i, num in enumerate(bank):
-            if first == 9 and sec == 9:
-                break
-            elif int(num) > first and i != last:
-                first = int(num)
-                sec = 0
-            elif int(num) > sec:
-                sec = int(num)
-
-        b.append(10 * first + sec)
+        num_arr:list[str] = ['0' for _ in range(12)]
+        last = len(bank) -1
+        for i,num in enumerate(bank):
+            for n in range(12):
+                if int(num) > int(num_arr[n]) and last - i >= 12-n-1:
+                    num_arr[n] = num
+                    num_arr[n+1:] = ['0' for _ in range(len(num_arr[n+1:]))] 
+                    break
+                elif i == last:
+                    if int(num) > int(num_arr[-1]):
+                        num_arr[-1] = num
+        
+        s = ''.join(num_arr)
+        b.append(int(s))
     return sum(b)
 
 
--- tests/test_day03.py
+++ tests/test_day03.py
@@ -14,4 +14,4 @@ def test1():
 
 
 def test2():
-    pass
+    assert part2(data) == 3121910778619
