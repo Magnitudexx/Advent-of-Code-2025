60cc3a4ade56cbd1099516cdf506ace4fd6a8f53
Philip Lundström <philip.lundstrom@live.se>
2025-12-04 20:28:38 +0100

    Part 1 done

---
+++ tests/test_day04.py
@@ -0,0 +1,23 @@
+from aoc.day04 import part1, part2, parse_input
+
+
+sample = """..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@."""
+
+data = sample.strip().splitlines()
+
+
+def test1():
+    assert part1(data) == 13
+
+
+def test2():
+    pass
