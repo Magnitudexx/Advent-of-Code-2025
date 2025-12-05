616fe868c9409987f8e0872478b61f375b7482ba
Philip Lundström <philip.lundstrom@live.se>
2025-12-05 10:05:32 +0100

    Done with day 5 part 1

---
+++ tests/test_day05.py
@@ -0,0 +1,23 @@
+from aoc.day05 import part1, part2, parse_input
+
+
+sample = """3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32"""
+data = sample.splitlines()
+
+
+def test1():
+    assert part1(data) == 3
+
+
+def test2():
+    pass
