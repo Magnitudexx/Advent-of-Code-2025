e1828595054a37a5249777a14138cd00d2714882
Magnitudexx <63145813+Magnitudexx@users.noreply.github.com>
2025-12-05 11:40:22 +0000

    Apply formatting

--- aoc/day05.py
+++ aoc/day05.py
@@ -1,18 +1,16 @@
-
-
 def parse_input(path: str) -> list[str]:
     with open(path) as f:
         return f.read().splitlines()
 
 
-def part1(data:list[str]):
-    idx = data.index('')
-    ranges:list[tuple[int,int]] = []
+def part1(data: list[str]):
+    idx = data.index("")
+    ranges: list[tuple[int, int]] = []
     for r in data[:idx]:
-        low, high = r.split('-')
+        low, high = r.split("-")
         ranges.append((int(low), int(high)))
 
-    ids:list[int] = [int(i) for i in data[idx+1:]]
+    ids: list[int] = [int(i) for i in data[idx + 1 :]]
     count = 0
     for i in ids:
         for low, high in ranges:
@@ -20,33 +18,36 @@ def part1(data:list[str]):
                 count += 1
                 break
     return count
+
+
 def part2(data):
-    idx = data.index('')
-    ranges:list[tuple[int,int]] = []
+    idx = data.index("")
+    ranges: list[tuple[int, int]] = []
     for r in data[:idx]:
-        low, high = r.split('-')
+        low, high = r.split("-")
         ranges.append((int(low), int(high)))
-    ranges.sort(reverse= True, key=lambda x: x[1])
-    p_ranges:list[tuple[int,int]] = []
+    ranges.sort(reverse=True, key=lambda x: x[1])
+    p_ranges: list[tuple[int, int]] = []
     count = 0
     overlap = False
     for low, high in ranges:
         for p_low, p_high in p_ranges:
-            #check if totally within a range
+            # check if totally within a range
             if low >= p_low and high <= p_high:
-                count -= high - low +1
+                count -= high - low + 1
                 break
-            #otherwise we may need to trim lower bound and higher bound
+            # otherwise we may need to trim lower bound and higher bound
             if low < p_low and high >= p_low:
-                high = p_low -1
+                high = p_low - 1
 
             if low <= p_high and high >= p_high:
-                low = p_high + 1 
-                
-        #when range is legal we add it to count
-        count += high - low +1
-        p_ranges.append((low,high))
+                low = p_high + 1
+
+        # when range is legal we add it to count
+        count += high - low + 1
+        p_ranges.append((low, high))
     return count
+
 
 if __name__ == "__main__":
     data = parse_input("input.txt")
