7f5bdf48094a9c88edef47f7c6d02f80f6591963
Philip Lundström <philip.lundstrom@live.se>
2025-12-05 12:39:11 +0100

    Day 5 part 2 done

--- aoc/day05.py
+++ aoc/day05.py
@@ -21,7 +21,32 @@ def part1(data:list[str]):
                 break
     return count
 def part2(data):
-    pass
+    idx = data.index('')
+    ranges:list[tuple[int,int]] = []
+    for r in data[:idx]:
+        low, high = r.split('-')
+        ranges.append((int(low), int(high)))
+    ranges.sort(reverse= True, key=lambda x: x[1])
+    p_ranges:list[tuple[int,int]] = []
+    count = 0
+    overlap = False
+    for low, high in ranges:
+        for p_low, p_high in p_ranges:
+            #check if totally within a range
+            if low >= p_low and high <= p_high:
+                count -= high - low +1
+                break
+            #otherwise we may need to trim lower bound and higher bound
+            if low < p_low and high >= p_low:
+                high = p_low -1
+
+            if low <= p_high and high >= p_high:
+                low = p_high + 1 
+                
+        #when range is legal we add it to count
+        count += high - low +1
+        p_ranges.append((low,high))
+    return count
 
 if __name__ == "__main__":
     data = parse_input("input.txt")
