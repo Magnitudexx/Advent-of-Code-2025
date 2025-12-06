751f2ff3b76d787d6169eeb51f730b5308552846
Philip Lundström <philip.lundstrom@live.se>
2025-12-06 10:53:44 +0100

    Day 6 part 1 done

---
+++ aoc/day06.py
@@ -0,0 +1,39 @@
+import re
+import numpy as np
+
+from aoc.utils import get_neighbors
+from numpy.typing import NDArray
+
+
+def parse_input(path: str) -> list[str]:
+    with open(path) as f:
+        return f.read().splitlines()
+
+
+def part1(data):
+    ops = re.findall(r"[+*]",data[-1])
+    data = data[:-1]
+    num_list = [re.findall(r"[0-9]+", l) for l in data]
+    arr: NDArray[np.str_] = np.array(num_list, dtype=str)
+    i_arr = arr.astype(int)
+    rows, cols = arr.shape
+    total = 0
+    for i, op in enumerate(ops):
+        col = i_arr[:,i]
+
+        match op:
+            case "+":
+                total += col.sum()
+            case "*":
+                total += col.prod()
+
+    return total
+
+def part2(data):
+    pass
+
+
+if __name__ == "__main__":
+    data = parse_input("input.txt")
+    print("Part 1:", part1(data))
+    print("Part 2:", part2(data))
