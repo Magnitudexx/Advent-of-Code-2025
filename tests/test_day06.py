787081d886e86b3046efb1a951c194bc7b7cd3fc
Philip Lundström <philip.lundstrom@live.se>
2025-12-06 10:53:44 +0100

    Day 6 part 1 done

---
+++ tests/test_day06.py
@@ -0,0 +1,16 @@
+from aoc.day06 import part1, part2, parse_input
+
+
+sample ="""123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  """ 
+data = sample.splitlines()
+
+
+def test1():
+    assert part1(data) == 4277556
+
+
+def test2():
+    pass
