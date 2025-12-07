f6feca7ef36256d5ffd3a5e032371632a79ec6ee
Magnitudexx <63145813+Magnitudexx@users.noreply.github.com>
2025-12-07 19:06:27 +0000

    Apply formatting

--- aoc/day07.py
+++ aoc/day07.py
@@ -8,41 +8,44 @@ def parse_input(path: str) -> list[str]:
         return f.read().splitlines()
 
 
-def part1(data:list[str]):
-    start = data[0].index('S')
+def part1(data: list[str]):
+    start = data[0].index("S")
     data = data[1:]
 
     idxs = set([start])
     total = 0
     for line in data:
-        np_line:NDArray[np.str_] = np.array(list(line))
-        splitter_idxs = np.where(np_line == '^')[0]
+        np_line: NDArray[np.str_] = np.array(list(line))
+        splitter_idxs = np.where(np_line == "^")[0]
 
         if splitter_idxs.size > 0:
             for idx in splitter_idxs:
                 if idx in idxs:
                     idxs.remove(idx)
-                    idxs.update([idx-1, idx+1])
+                    idxs.update([idx - 1, idx + 1])
                     total += 1
 
     return total
 
+
 from collections import defaultdict
+
+
 def part2(data):
-    start = data[0].index('S')
+    start = data[0].index("S")
     data = data[1:]
 
-    idxs:dict[int,int] = defaultdict(int)
+    idxs: dict[int, int] = defaultdict(int)
     idxs[start] = 1
     for line in data:
-        np_line:NDArray[np.str_] = np.array(list(line))
-        splitter_idxs = np.where(np_line == '^')[0]
+        np_line: NDArray[np.str_] = np.array(list(line))
+        splitter_idxs = np.where(np_line == "^")[0]
 
         if splitter_idxs.size > 0:
             for idx in splitter_idxs:
                 if idx in idxs:
-                    idxs[idx-1] += idxs[idx]
-                    idxs[idx+1] += idxs[idx]
+                    idxs[idx - 1] += idxs[idx]
+                    idxs[idx + 1] += idxs[idx]
             for idx in splitter_idxs:
                 idxs[idx] = 0
 
